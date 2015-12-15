c08aeba7960447f066c79205eacd029afa9f3a90
teo <teo.ionescu@gmail.com>
2015-12-15 03:37:15 +0200

    added intro scene

---
+++ JLD/JLD/Assets/Scripts/Intro.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/*
+* Intro menu
+**/
+public class Intro : MonoBehaviour
+{
+    public Image logo;
+    public Image splash;
+    public Button play;
+
+    public float timer = 0;
+    
+	void Update ()
+    {
+	    if (timer < 3)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 3)
+            {
+                logo.gameObject.SetActive(false);
+                splash.gameObject.SetActive(true);
+                play.gameObject.SetActive(true);
+            }
+        }
+	}
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
